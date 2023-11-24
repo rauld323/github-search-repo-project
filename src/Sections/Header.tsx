d4d77df769b165c89451a82e9c5db75e31512f14
Raul Davila <rauldavila323@gmail.com>
2023-11-24 10:39:16 +0100

    fix margin in header text and initial screen

--- src/Sections/Header.tsx
+++ src/Sections/Header.tsx
@@ -9,11 +9,11 @@ interface IProps {
 
 const Header: FC<IProps> = ({ userName, setUserName, fetchRepos }) => {
   return (
-    <div className="flex item-center items-center w-full flex-wrap md:h-24 h-32 border-b border-gray-100 bg-white">
-      <div className="w-full md:w-1/2 text-3xl font-bold text-center md:text-left md:pl-10 pl-0 md:mt-0 mt-3">
+    <div className="flex item-center items-center w-full flex-wrap md:h-24 h-36 border-b border-gray-100 bg-white">
+      <div className="w-full md:w-1/2 text-3xl font-bold text-center md:text-left md:pl-10 pl-0 md:mt-0 mt-6">
         GitHub Searcher
       </div>
-      <div className="w-full md:w-1/2 flex flex-wrap gap-4 flex-col items-center md:items-start mt-1 md:mt-0 mb-3 md:mb-0 md:pr-10 pr-0">
+      <div className="w-full md:w-1/2 flex flex-wrap gap-4 flex-col items-center md:items-start mt-1 md:mt-0 mb-6 md:mb-0 md:pr-10 pr-0">
         <SearchBar
           userName={userName}
           setUserName={setUserName}
